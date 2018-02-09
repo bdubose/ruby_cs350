e6323bf4772fcb32e8306ed22d24b86cba0c9210
Branwin DuBose <bdubose@doozer.com>
2018-02-08 19:24:50 -0600

    adding driver stuff

--- driver.rb
+++ driver.rb
@@ -1,16 +1,22 @@
 # Branwin DuBose, Josh Echols
-#
+# CS 401
+# Ruby Assignment
 
-
-
-
-# This program takes one argument (the file name)
+# This driver program takes one argument (the file name)
 filename = ARGV[0]
 
 if ARGV.length == 1
   if File.exist? filename
     puts 'File found!'
-    analyzer = Analyzer.new(filename)
+
+    string_contents = ''
+    file = File.open(filename)
+
+    file.each_line do |line|
+      string_contents += line
+    end
+
+    .new(string_contents)
   else
     puts 'ERROR: The given filename does not exist.'
   end
