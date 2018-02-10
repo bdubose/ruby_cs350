1ff063bcc36ae458287c622253243917a292bfa7
Branwin DuBose <bdubose@doozer.com>
2018-02-09 19:04:01 -0600

    added symbols boolean methods to analyzer

--- analyzer.rb
+++ analyzer.rb
@@ -9,26 +9,99 @@ class Analyzer
 
   # TODO: not sure if this is going to be right
   def valid_stmts?
-      and_all?(valid_stmt?, semicolon?) \
-      or \
-      and_all?(valid_stmt?, semicolon?, valid_stmt?)
+      (valid_stmt? and semi_colon?)                   \
+      or                                              \
+      (valid_stmt? and semi_colon? and valid_stmts?)
   end
 
   def valid_stmt?
 
   end
 
-  def semi_colon?
-    @tokenizer.get_token_kind = Symbol::SYMBOLS[:SEMI_COL]
+
+  #region SYMBOLS
+  def add_op?
+    @tokenizer.getKind = Symbol::SYMBOLS[:ADD_OP]
   end
 
-  # and_all? takes variable number of boolean expressions and aggregates them into
-  # a final answer
-  def and_all? *exprs
-    ret = true
-    for exp in exprs
-      ret = (ret and exp)
-    end
-    ret
+  def mul_op?
+    @tokenizer.getKind = Symbol::SYMBOLS[:MUL_OP]
   end
+
+  def integer?
+    @tokenizer.getKind = Symbol::SYMBOLS[:INTEGER]
+  end
+
+  def identifier?
+    @tokenizer.getKind = Symbol::SYMBOLS[:IDENTIFIER]
+  end
+
+  def open_paren?
+    @tokenizer.getKind = Symbol::SYMBOLS[:OPEN_PAREN]
+  end
+
+  def close_paren?
+    @tokenizer.getKind = Symbol::SYMBOLS[:CLOSE_PAREN]
+  end
+
+  def lang_if?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_IF]
+  end
+
+  def lang_then?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_THEN]
+  end
+
+  def lang_end?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_END]
+  end
+
+  def lang_for?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_FOR]
+  end
+
+  def lang_to?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_TO]
+  end
+
+  def lang_from?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_FROM]
+  end
+
+  def lang_else?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_ELSE]
+  end
+
+  def lang_do?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_DO]
+  end
+
+  def lang_by?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_BY]
+  end
+
+  def lang_true?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_TRUE]
+  end
+
+  def lang_false?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_FALSE]
+  end
+
+  def lang_not?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LANG_NOT]
+  end
+
+  def log_rel_op?
+    @tokenizer.getKind = Symbol::SYMBOLS[:LOG_REL_OP]
+  end
+
+  def assign_op?
+    @tokenizer.getKind = Symbol::SYMBOLS[:ASSIGN_OP]
+  end
+
+  def semi_colon?
+    @tokenizer.getKind = Symbol::SYMBOLS[:SEMI_COL]
+  end
+  #endregion
 end
